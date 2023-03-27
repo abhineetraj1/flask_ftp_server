2bb63f88ae1ad70e6dec4a50c3c6c0c6eb71300c
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2023-03-27 13:13:50 +0530

    Updated functionality and design
    -Removed unwanted messy lines
    -Updated structures
    -Modified syntax

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
 from flask import*
+from flask import render_template
 import datetime
 import os
 import shutil
 import smtplib
 
-app = Flask(__name__,static_folder="f")
+app = Flask(__name__,static_folder="f", template_folder=os.getcwd())
 
 def rdm():
 	return str(datetime.datetime.now()).replace(" ","").replace(":","").replace(".","").replace("-","")
-def rf(a):
-	return open(a,"r").read()
 
-def scp(a):
-	return """<center><div id="alert-box">"""+a+"""<br><button id="ok-btn-alert" onclick="document.getElementById('alert-box').remove()">Ok</button><style type="text/css">#alert-box{border-radius: 10px;border: solid;border-color:red;color:red;background:white;text-align:center;margin:1vh;padding:1vh;font-size:3vh;}#ok-btn-alert{font-size:3vh;color:white;width:40%;background:red;border:solid;text-align:center;padding:2px;}</style><script type="text/javascript">if(window.innerHeight > window.innerWidth) {document.getElementById('alert-box').style.width="90%";} else {document.getElementById('alert-box').style.width=window.innerWidth/3+"px";}</script></div></center>"""
 
 @app.route("/",methods=["GET","POST"])
 def aa2():
 	if (request.method == "GET"):
-		return rf("s.html")
+		return render_template("s.html", msg="none")
 	else:
 		files = request.files.getlist('files[]')
 		n = rdm()
 		os.mkdir(n)
 		for file in files:
 			file.save(n+"/"+file.filename)
-		return rf("a.html")
+		return render_template("s.html", msg="File uploaded successfully")
 
 @app.errorhandler(404)
 def kll(e):
-	return rf("404.html")
+	return render_template("err.html", error="404 page not found")
 
 @app.errorhandler(Exception)
 def report_bug(n):
 	open("bugs.txt","a").write("\n"+str(n)+"\n")
-	return rf("err.html")
+	return render_template("err.html", error="500 Internal server error")
 
 if __name__ == '__main__':
 	app.run()
